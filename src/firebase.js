9cf8cbc660482c8623524e79bac072298ad5eac2
dhruvpal05 <dhruv416pal@gmail.com>
2024-06-06 22:20:17 +0530

    firebase auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCGlQcborYqpAlhTfptlGsJ4kQKh1VhZfg",
+  apiKey: "AIzaSyCGlQcborYqpAlhTfptlGsJ4kQKh1VhZfg-w0qYtxGyaOGQquBs",
   authDomain: "gymate-ff113.firebaseapp.com",
   projectId: "gymate-ff113",
   storageBucket: "gymate-ff113.appspot.com",
@@ -16,6 +12,6 @@ const firebaseConfig = {
   measurementId: "G-224CZNPTHN"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
